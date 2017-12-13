a4630c9acbe24b0a385a478b9a0033243549f119
Stephan Unverwerth <s.unverwerth@gmx.de>
2017-12-14 00:20:57 +0100

    3d

--- assets/fs.glsl
+++ assets/fs.glsl
@@ -1,8 +1,16 @@
 #version 330 core
-out vec4 FragColor;  
+
 in vec3 ourColor;
+in vec2 TexCoord;
+
+out vec4 FragColor;  
   
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
 void main()
 {
-    FragColor = vec4(ourColor, 1.0);
+	vec4 t1 = texture(texture1, TexCoord);
+	vec4 t2 = texture(texture2, TexCoord);
+    FragColor = mix(t1, t2, t2.a);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "stb_image.h"
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <cmath>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 std::string readFile(const char* name) {
 	std::ifstream t(name);
@@ -17,8 +23,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
 // settings
-const unsigned int SCR_WIDTH = 100;
-const unsigned int SCR_HEIGHT = 100;
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 800;
 
 class GLShader {
 public:
@@ -59,6 +65,31 @@ public:
 		glShaderSource(handle, 1, &str, NULL);
 		glCompileShader(handle);
 	}
+};
+
+class GLVertexBuffer {
+public:
+	GLVertexBuffer() {
+		glGenBuffers(1, &handle);
+	}
+
+	~GLVertexBuffer() {
+		glDeleteBuffers(1, &handle);
+	}
+
+	void bind() {
+		glBindBuffer(GL_ARRAY_BUFFER, handle);
+	}
+
+	void unbind() {
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+
+	void setData(GLsizeiptr size, const void* data, GLenum usage) {
+		glBufferData(GL_ARRAY_BUFFER, size, data, usage);
+	}
+
+	GLuint handle;
 };
 
 class GLProgram {
@@ -102,34 +133,9 @@ public:
 	GLuint handle;
 };
 
-class GLVertexBuffer {
-public:
-	GLVertexBuffer() {
-		glGenBuffers(1, &handle);
-	}
-
-	~GLVertexBuffer() {
-		glDeleteBuffers(1, &handle);
-	}
-
-	void bind() {
-		glBindBuffer(GL_ARRAY_BUFFER, handle);
-	}
-
-	void unbind() {
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-	}
-
-	void setData(GLsizeiptr size, const void* data, GLenum usage) {
-		glBufferData(GL_ARRAY_BUFFER, size, data, usage);	
-	}
-
-	GLuint handle;
-};
-
 class GLVertexArray {
 public:
-	GLVertexArray(GLVertexBuffer* buffer): buffer(buffer) {
+	GLVertexArray(GLVertexBuffer* buffer) : buffer(buffer) {
 		glGenVertexArrays(1, &handle);
 	}
 
@@ -147,6 +153,7 @@ public:
 
 	GLVertexBuffer* buffer;
 	GLuint handle;
+
 };
 
 class GLElementBuffer {
@@ -176,7 +183,7 @@ public:
 
 class GLMesh {
 public:
-	GLMesh(GLVertexArray* vertices, GLElementBuffer* indices): vertices(vertices), indices(indices) {
+	GLMesh(GLVertexArray* vertices, GLElementBuffer* indices) : vertices(vertices), indices(indices) {
 	}
 
 	void bind() {
@@ -186,21 +193,84 @@ public:
 
 	GLVertexArray* vertices;
 	GLElementBuffer* indices;
+
+};
+
+class GLTexture {
+public:
+	unsigned int handle;
 };
 
 class GL {
 public:
+	int width;
+	int height;
+
 	void clearColor(GLfloat r, GLfloat g, GLfloat b, GLfloat a) {
 		glClearColor(r, g, b, a);
-		glClear(GL_COLOR_BUFFER_BIT);		
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 
 	void viewport(GLint x, GLint y, GLsizei width, GLsizei height) {
+		width = width;
+		height = height;
 		glViewport(0, 0, width, height);
 	}
 
 	void drawIndexed(GLenum mode, GLsizei count, GLenum type, const void* indices) {
 		glDrawElements(mode, count, type, indices);
+	}
+
+	void use(GLProgram* program) {
+		glUseProgram(program->handle);
+	}
+
+	GLTexture* loadTexture2D(const char* name) {
+		unsigned int handle;
+		glGenTextures(1, &handle);
+		glBindTexture(GL_TEXTURE_2D, handle);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_set_flip_vertically_on_load(true);
+
+		int width, height, nrChannels;
+		unsigned char *data = stbi_load(name, &width, &height, &nrChannels, 0);
+		if (!data) {
+			glDeleteTextures(1, &handle);
+			return nullptr;
+		}
+
+		unsigned int format;
+		switch (nrChannels) {
+		case 4:
+			format = GL_RGBA;
+			break;
+		case 3:
+		default:
+			format = GL_RGB;
+		}
+
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		stbi_image_free(data);
+
+		auto texture = new GLTexture();
+		texture->handle = handle;
+		return texture;
+	}
+
+	void bind(GLTexture* texture, int slot) {
+		glActiveTexture(GL_TEXTURE0 + slot);
+		glBindTexture(GL_TEXTURE_2D, texture->handle);
+	}
+
+	void bind(GLMesh* mesh) {
+		mesh->bind();
 	}
 };
 
@@ -239,6 +309,11 @@ int main()
 		return -1;
 	}
 
+	gl.width = SCR_WIDTH;
+	gl.height = SCR_HEIGHT;
+
+	glEnable(GL_DEPTH_TEST);
+
 	// vertex shader
 	auto vs = GLVertexShader(readFile("assets/vs.glsl"));
 	if (!vs.isSuccess())
@@ -270,19 +345,54 @@ int main()
 	VBO.bind();
 
 	float vertices[] = {
-		0.5f,  0.5f, 0.0f,	1.0f, 0.0f, 0.0f,  // top right
-		0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f, // bottom right
-		0.0f, -1.0f, 0.0f,	0.0f, 1.0f, 0.0f, // bottom middle
-		-0.5f, -0.5f, 0.0f,	0.0f, 0.0f, 1.0f, // bottom left
-		-0.5f,  0.5f, 0.0f,	1.0f, 0.0f, 1.0f, // top left 
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
 	VBO.setData(sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
 	VBO.unbind();
@@ -294,7 +404,6 @@ int main()
 	unsigned int indices[] = {  // note that we start from 0!
 		0, 1, 2,   // first triangle
 		0, 2, 3,    // second triangle
-		0, 3, 4    // second triangle
 	};
 
 	EBO.setData(sizeof(indices), indices, GL_STATIC_DRAW);
@@ -305,6 +414,15 @@ int main()
 
 	// uncomment this call to draw in wireframe polygons.
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+
+	program.use(); // don't forget to activate/use the shader before setting uniforms!
+
+	auto texture1 = gl.loadTexture2D("assets/container.jpg");
+	auto texture2 = gl.loadTexture2D("assets/awesomeface.png");
+
+	glUniform1i(glGetUniformLocation(program.handle, "texture1"), 0);
+	glUniform1i(glGetUniformLocation(program.handle, "texture2"), 1);
 
 	// render loop
 	// -----------
@@ -318,15 +436,33 @@ int main()
 		// ------
 		gl.clearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-		// draw our first triangle
-		program.use();
+		gl.use(&program);
+		gl.bind(texture1, 0);
+		gl.bind(texture2, 1);
 
-		float timeValue = glfwGetTime();
-		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-		program.setUniform4f("ourColor", 0.0f, greenValue, 0.0f, 1.0f);
+		glm::mat4 world(1.0f);
+		glm::mat4 view(1.0f);
+		glm::mat4 projection(1.0f);
 
-		mesh.bind();
-		gl.drawIndexed(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		world = glm::rotate(world, (float)glfwGetTime(), glm::vec3(1.0f, 0.0f, 0.0f));
+		world = glm::rotate(world, (float)glfwGetTime()*0.31f, glm::vec3(0.0f, 1.0f, 0.0f));
+		world = glm::rotate(world, (float)glfwGetTime()*2.7f, glm::vec3(0.0f, 0.0f, 1.0f));
+
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		projection = glm::perspective(glm::radians(45.0f), (float)gl.width / (float)gl.height, 0.1f, 100.0f);
+
+		unsigned int index = glGetUniformLocation(program.handle, "worldMatrix");
+		glUniformMatrix4fv(index, 1, GL_FALSE, glm::value_ptr(world));
+
+		index = glGetUniformLocation(program.handle, "viewMatrix");
+		glUniformMatrix4fv(index, 1, GL_FALSE, glm::value_ptr(view));
+
+		index = glGetUniformLocation(program.handle, "projectionMatrix");
+		glUniformMatrix4fv(index, 1, GL_FALSE, glm::value_ptr(projection));
+
+		gl.bind(&mesh);
+		//gl.drawIndexed(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		// glBindVertexArray(0); // no need to unbind it every time 
 
